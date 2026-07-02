46537f2205ea14c7e2b72f08a07eaa2e65d99b0e
matmacote <mat.macote@lgusanfernandobuk.gov.ph>
2026-07-02 17:46:06 +0800

    Fix admin login API URL

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   e.preventDefault();
 
   try {
-    const response = await fetch("http://localhost:5000/api/users/login", {
+    const response = await fetch("https://tesda-opcr-dashboard.onrender.com/api/users/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
