37beb9864eb88c3dc83de4f16e348a132d4ec100
matmacote <mat.macote@lgusanfernandobuk.gov.ph>
2026-07-09 18:02:11 +0800

    Secure authentication using bcrypt V2

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const mysql = require("mysql2");
 
-const db = mysql.createPool({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
@@ -9,7 +9,10 @@ const db = mysql.createPool({
   database: process.env.DB_NAME,
   ssl: {
     rejectUnauthorized: false
-  }
+  },
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-module.exports = db;
+module.exports = pool.promise();
